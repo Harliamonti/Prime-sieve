029a1f252f898b6adafa1798c30fc81883d0ad70
Harliamonti <123170910+Harliamonti@users.noreply.github.com>
2023-03-03 20:13:02 +0200

    Add basic files

---
+++ build-prime-sieve-Desktop-Debug/CMakeFiles/prime-sieve.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prime-sieve.dir/main.cpp.o"
+  "CMakeFiles/prime-sieve.dir/main.cpp.o.d"
+  "prime-sieve"
+  "prime-sieve.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prime-sieve.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
