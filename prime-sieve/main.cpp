8c060f1cf8bd24f0ab022f9687e5a00edfab2812
Harliamonti <123170910+Harliamonti@users.noreply.github.com>
2023-03-04 16:59:38 +0200

    Fix bug

--- prime-sieve/main.cpp
+++ prime-sieve/main.cpp
@@ -8,24 +8,22 @@ void decoder(std::vector<bool> &vec);
 
 int main()
 {
-    NUM max{ 1'000'000'000 }, currentMultiple{ 3 }, vecStartPos{}, limit{ static_cast<NUM>(std::sqrt(max)) };
+    NUM max{ 1000 }, currentMultiple{ 3 }, vecStartPos{}, limit{ static_cast<NUM>(std::sqrt(max)) };
     const NUM ARR_MAX{ max/2 };
     std::vector<bool> primes(ARR_MAX, 1);
 
-    while(currentMultiple < limit) {
+    while(currentMultiple <= limit) {
         NUM i{vecStartPos + currentMultiple};
         if (primes.at(vecStartPos) == 1) {
             for(NUM j{ i }; j < ARR_MAX; j += currentMultiple) {
                 primes.at(j) = false;
             }
         }
-
-
         currentMultiple += 2;
         vecStartPos++;
     }
 
-//    decoder(primes);
+    decoder(primes);
 }
 
 void decoder(std::vector<bool> &vec) {
